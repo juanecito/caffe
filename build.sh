b251fcf7e30c53752bf44070b338f449c6d55478
juanecito <juanecitorr@gmail.com>
2017-07-17 18:38:43 +0200

    Compilation error correction, set compilators version

--- build.sh
+++ build.sh
@@ -4,11 +4,14 @@ ARGS=$@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+export CC=/usr/bin/gcc-5
+export CXX=/usr/bin/g++-5
+
 cd $DIR
 
 mkdir -p ./build
 cd ./build
-cmake ../
+cmake -D CMAKE_C_COMPILER=/usr/bin/gcc-5 -D CMAKE_CXX_COMPILER=/usr/bin/g++-5 ../
 make -j 20
 cd ..
 
