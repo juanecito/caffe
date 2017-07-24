7f44b3affe3f7945981927156d8442f0b54f34c4
juanecito <juanecitorr@gmail.com>
2017-07-24 02:44:06 +0200

    Debug library option

--- build.sh
+++ build.sh
@@ -9,10 +9,19 @@ export CXX=/usr/bin/g++-5
 
 cd $DIR
 
-mkdir -p ./build
-cd ./build
-cmake -D CMAKE_C_COMPILER=/usr/bin/gcc-5 -D CMAKE_CXX_COMPILER=/usr/bin/g++-5 ../
-make -j 20
+BUILD_DEBUG="-DCMAKE_BUILD_TYPE=Debug"
+BUILD_RELEASE="-DCMAKE_BUILD_TYPE=Release"
+
+mkdir -p ./build_d
+cd ./build_d
+cmake -D CMAKE_C_COMPILER=/usr/bin/gcc-5 -D CMAKE_CXX_COMPILER=/usr/bin/g++-5 ${BUILD_DEBUG} ../
+make -j 4
+
+cd ..
+mkdir -p ./build_r
+cd ./build_r
+cmake -D CMAKE_C_COMPILER=/usr/bin/gcc-5 -D CMAKE_CXX_COMPILER=/usr/bin/g++-5 ${BUILD_RELEASE} ../
+make -j 4
 cd ..
 
 
